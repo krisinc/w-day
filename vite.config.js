0eba0ff972fdbd8793922be3d56d892f71796335
Clark <clark.huang@timecitytech.com>
2025-10-16 18:39:07 +0800

    (fix) base url, add env

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { loadEnv } from 'vite'
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.BASE || '/w-day/',
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+// export default defineConfig({
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    base: process.env.VITE_BASE_URL || '/w-day/',
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [vue()],
-})
+    plugins: [vue()],
+  })
+}
